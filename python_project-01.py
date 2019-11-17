25e022b3f9b788534f0507c53a610a6ad116bf73
zionHuang <hjszion@gmail.com>
2019-11-17 00:15:46 -0800

    update

---
+++ python_project-01.py
@@ -0,0 +1,10 @@
+character_name = "Tom"
+character_age = "50"
+
+print("There once was a man named " + character_name + ",")
+print("He was " + character_age + "years old" + ".")
+
+character_name = "Mike"
+
+print("He really liked the name " + character_name + ",")
+print("But didn't like being " + character_age + ".")
