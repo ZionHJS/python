ca14b7d7a876873c01f7005cd10e922cb29c86e0
zionHuang <hjszion@gmail.com>
2019-11-17 22:47:18 -0800

    udapte

--- statement.py
+++ statement.py
@@ -12,3 +12,12 @@ else:
    print("You are not a male")
    print("You are not tall")
 
+def max_num(num1, num2, num3):
+   if num1 >= num2 and num1 >= num3:
+      return num1
+   elif num2 >= num1 and num2 >= num3:
+      return num2
+   else:
+      return num3
+   
+print(max_num(3,4,5))
