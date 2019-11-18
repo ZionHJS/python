c97f87a46421496b40cad403008a5cbeb687a859
zionHuang <hjszion@gmail.com>
2019-11-17 23:00:26 -0800

    update

---
+++ dictionary.py
@@ -0,0 +1,19 @@
+monthConversions = {
+    "Jan":"January",
+"Feb":"February",
+"Mar":"March",
+"Apr":"April",
+"May":"May",
+"Jun":"June",
+"Jul":"July",
+"Aug":"August",
+"Sep":"September",
+"Oct":"October",
+"Nov":"November",
+"Dec":"December",
+}
+
+print(monthConversions["Nov"])
+print(monthConversions.get("Nov"))
+print(monthConversions.get('lk'))
+print(monthConversions.get("luk", "Not a Valid-Key"))
