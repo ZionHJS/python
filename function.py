7692227dcbbf13973829843db23f22153004166a
zionHuang <hjszion@gmail.com>
2019-11-17 22:22:38 -0800

    update

--- function.py
+++ function.py
@@ -1,6 +1,7 @@
-def say_hi(name):
-    print("hello " + name)
+def say_hi(name, age):
+    print("hello " + name + ", you're " + str(age))
 
 print("Top")
-say_hi("Mike")
+say_hi("Mike", "35")
 print("Bottom")
+say_hi("Steve", "70")
