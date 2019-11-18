c031a755d6b665866ca39d6073cf58775123026a
zionHuang <hjszion@gmail.com>
2019-11-17 21:21:44 -0800

    update

--- python_project-01.py
+++ python_project-01.py
@@ -1,5 +1,13 @@
 character_name = "Tom"
-character_age = "50"
+character_age = 50
+isMale = True
+
+phrase = "Giraffe Academy"
+
+print("Giraffe\"Academy")
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[0])
 
 print("There once was a man named " + character_name + ",")
 print("He was " + character_age + "years old" + ".")
