d11f999338c82ae5962635f3dece5df56b77fb36
zionHuang <hjszion@gmail.com>
2019-11-17 22:44:30 -0800

    update

--- statement.py
+++ statement.py
@@ -1,5 +1,14 @@
-is_male = False
+is_male = True
+is_tall = True
 
-if is_male:
+if is_male or is_tall:
    print("You are a male")
-   
+   print("You are tall")
+elif is_male and not(is_tall):
+   print("You are a short male")
+elif not(is_male) and is_tall:
+   print("You are not a male but are tall")
+else:
+   print("You are not a male")
+   print("You are not tall")
+
