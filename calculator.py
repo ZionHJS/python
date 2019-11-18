f572b12f2639895161ee492ef7e061f340dc8f4c
zionHuang <hjszion@gmail.com>
2019-11-17 21:54:12 -0800

    update

--- calculator.py
+++ calculator.py
@@ -1,5 +1,13 @@
 num1 = input("Enter a number: ")
 num2 = input("Enter antoher number: ")
-result = int(num1) + int(num2)
+result = int(num1) + float(num2)
 
 print(result)
+
+color = input('Enter a color:')
+plural_noun = input("Enter a Plural Noun:")
+celebrity = input("Enter a celebrity:")
+
+print("Rose are {color}")
+print("{plural noun} are blue")
+print("I lvoe {celebrity}")
