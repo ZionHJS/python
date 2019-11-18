260496fada1fa207242fc18db2492348a8ad3f76
zionHuang <hjszion@gmail.com>
2019-11-17 22:03:02 -0800

    update

--- calculator.py
+++ calculator.py
@@ -11,3 +11,9 @@ celebrity = input("Enter a celebrity:")
 print("Rose are {color}")
 print("{plural noun} are blue")
 print("I lvoe {celebrity}")
+
+
+friends = ["Kevin", "J", False, "Oscal", "Toby"]
+friends[1] = "Mike"
+print(friends[0:4])
+print(friends[1])
