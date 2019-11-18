ab51320b65ddf772846bf983d1b9b3806ff1623d
zionHuang <hjszion@gmail.com>
2019-11-17 22:52:38 -0800

    update

--- statement.py
+++ statement.py
@@ -19,5 +19,20 @@ def max_num(num1, num2, num3):
       return num2
    else:
       return num3
-   
+
 print(max_num(3,4,5))
+
+num1 = float(input("Enter first number:"))
+op = input("Enter operator")
+num2 = float(input("Enter second number:"))
+
+if op == "+":
+   print(num1 + num2)
+elif op == "-":
+   print(num1 - num2)
+elif op == "/":
+   print(num1 / num2)
+elif op == "*":
+   print(num1 * num2)
+else:
+   print("Invalid Operator")
