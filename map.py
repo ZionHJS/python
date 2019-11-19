5aa44d18e902592c6dd2988f3aef427945b35899
zionHuang <hjszion@gmail.com>
2019-11-18 22:39:43 -0800

    update

--- map.py
+++ map.py
@@ -9,6 +9,15 @@ asd
 as
 a
 '''
-
 #This program is cool
+
 print("Comments are fun")
+
+try:
+    value = 10 / 0
+    number = int(input("Enter a number:"))
+    print(number)
+except ZeroDivisionError as err:
+    print(err)
+except ValueError:
+    print("invalid input")
