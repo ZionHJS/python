52a67500ce4e5efeb18f51d79537425412d4c55b
zionHuang <hjszion@gmail.com>
2019-11-18 22:50:29 -0800

    update

--- map.py
+++ map.py
@@ -21,3 +21,9 @@ except ZeroDivisionError as err:
     print(err)
 except ValueError:
     print("invalid input")
+
+employee_file = open("employees.txt", "r")
+for employee in employee_file.readlines():
+    print(employee)
+print(employee_file.readlines()[1])
+employee_file.close()
